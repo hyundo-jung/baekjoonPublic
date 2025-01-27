9785c73a2eb54637600592b1382a5970d3cab88c
Hyundo Jung <117788231+hyundo-jung@users.noreply.github.com>
2025-01-27 12:01:45 -0600

    Create 24313.c

---
+++ 2025-01/24313.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a1, a0, c, n0;
+
+    scanf("%d %d", &a1, &a0);
+    scanf("%d", &c);
+    scanf("%d", &n0);
+
+    // if (a1 > c && a0 < 0)
+    // {
+    //     printf("0 \n");
+    // }
+    // else if (a1 * n0 + a0 <= c * n0)
+    //     printf("1 \n");
+    // else
+    //     printf("0 \n");
+
+    if (a1 > c)
+    {
+        printf("0 \n");
+    }
+    else if (a1 == c)
+    {
+        if (a0 > 0)
+        {
+            printf("0 \n");
+        }
+        else
+        {
+            printf("1 \n");
+        }
+    }
+    else if (a1 < c)
+    {
+
+        if (a1 * n0 + a0 <= c * n0)
+        {
+            printf("1 \n");
+        }
+        else
+        {
+            printf("0 \n");
+        }
+    }
+
+    return 0;
+}
