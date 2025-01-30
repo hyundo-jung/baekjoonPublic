a43ef6b8b0a4dc64f22214866ba7db47b2bf1bbf
Hyundo Jung <117788231+hyundo-jung@users.noreply.github.com>
2025-01-30 12:00:23 -0600

    Create 1436.c

---
+++ 2025-01/1436.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(void)
+{   
+    int N, val = 665, temp, num, consec, count = 0;
+
+    scanf("%d", &N);
+
+
+    while (N > count)
+    {
+        val++;
+        consec = 0;
+
+        temp = val;
+
+        while (temp > 0)
+        {
+            if (temp % 10 == 6)
+            {
+                if (consec == 0)
+                {
+                    consec = 1;
+                }
+                else
+                {
+                    consec++;
+                }
+
+                if (consec == 3)
+                {
+                    count++;
+                    break;
+                }
+            }
+            else
+            {
+                consec = 0;
+            }
+
+            temp /= 10;
+        }
+
+    }
+    
+    printf("%d \n", val);
+    
+    return 0;
+}
